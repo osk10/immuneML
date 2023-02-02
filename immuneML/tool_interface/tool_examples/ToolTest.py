65c7a3267ad1f2a89b165378ea4dcab494866a54
jorgeski <jorgeski@student.matnat.uio.no>
2023-02-02 17:40:05 +0100

    Removed unused function in ToolTest.py

--- immuneML/tool_interface/tool_examples/ToolTest.py
+++ immuneML/tool_interface/tool_examples/ToolTest.py
@@ -1,7 +1,6 @@
 import sys
 import time
 import socket
-from multiprocessing.connection import Client
 
 
 class ToolTest:
@@ -30,29 +29,6 @@ class ToolTest:
 
         client_socket.send('stop connection'.encode())
         client_socket.close()
-
-    @staticmethod
-    def main2(json_data):
-        print(f"Data in tool: {json_data}")
-
-        # Start waiting for communication
-        # Tool will send a request through localhost when it is started by immuneML
-
-        active_connection = Client(('localhost', 6000), authkey=b'123')
-
-        test_counter = 0
-        test_counter_limit = 10
-        running = True
-        while running:
-            print(f"Running tool iteration {test_counter}")
-            active_connection.send('test')
-            time.sleep(1)
-            test_counter += 1
-            if test_counter >= test_counter_limit:
-                running = False
-
-        active_connection.send("close connection")
-        active_connection.close()
 
     @staticmethod
     def testing_func():
