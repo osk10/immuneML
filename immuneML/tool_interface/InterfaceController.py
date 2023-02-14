e28f98db67e3867e6ba576e6323ab31c6b363c5e
jorgeski <jorgeski@student.matnat.uio.no>
2023-02-14 13:28:05 +0100

    Removed socket related code

--- immuneML/tool_interface/InterfaceController.py
+++ immuneML/tool_interface/InterfaceController.py
@@ -1,11 +1,5 @@
-import os
-import time
-import socket
-from multiprocessing.connection import Listener
-
 from immuneML.tool_interface.ToolType import ToolType
 from immuneML.tool_interface.InterfaceObject import InterfaceObject
-from pathlib import Path
 import subprocess
 
 
@@ -26,36 +20,12 @@ class InterfaceController:
 
     @staticmethod
     def _start_subprocess(ml_specs: dict):
-        #  Specify socket information
-        server_socket = socket.socket()
-        host = socket.gethostname()
-        port = 6000
-
-        #  Bind host and porth number
-        server_socket.bind((host, port))
-
         #  Define and run subprocess (external tool)
         file = ml_specs.get("tool_path") + "/" + ml_specs.get("tool_execution_file")
         json_data_example = InterfaceController._create_JSON_data()
         proc = subprocess.Popen(["python", file, json_data_example],
                                 stdout=subprocess.PIPE,
                                 stdin=subprocess.PIPE)
-
-        #  Listen for connections. Only listen to one client (tool) at a time
-        #  Can be increased if we need multiple tools to run at the same time
-        #  Tried to do this before starting subprocess, but this created a deadlock
-        server_socket.listen(1)
-        conn, address = server_socket.accept()
-
-        #  Handle data from connected client (tool)
-        while True:
-            data = conn.recv(1024).decode()
-            print(f"Data received from client: {data}")
-            if data == 'EXIT CONNECTION':
-                break
-
-        conn.send('Closing connection'.encode())
-        conn.close()
 
         #  Printing the output that the tool gives while running on its own side
         output_list = proc.communicate()[0].decode('UTF-8')
